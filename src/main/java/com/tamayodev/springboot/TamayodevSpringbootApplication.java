bf1041146ffffbeed1a9eb6ef06e5b0e8285a140
H. Tamayo <hntamayo1@gmail.com>
2022-03-04 23:26:33 -0500

    COMMENT

---
+++ src/main/java/com/tamayodev/springboot/TamayodevSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tamayodev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamayodevSpringbootApplication {
+	// COMMENT
+	public static void main(String[] args) {
+		SpringApplication.run(TamayodevSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamayodev/springboot/TamayodevSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamayodev.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamayodevSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
